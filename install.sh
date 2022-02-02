63633e5383f6e1442d24fb9197cb066da3c74342
zerbini.roberto@gmail.com <-?x0#Szs1W?EICWl>
2022-02-02 18:21:41 +0400

    Resnet working

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ ACCOUNT=$(gcloud config list account --format "value(core.account)")
 NUM_NODES=1
 
 sed -i -- 's/<PROJECT_ID>/'"$PROJECT_ID"'/g' tfserving_deployment.yaml
-sed -i -- 's/<PROJECT_ID>/'"$APP_NAME"'/g' tfserving_deployment.yaml
+sed -i -- 's/<APP_NAME>/'"$APP_NAME"'/g' tfserving_deployment.yaml
 
 gcloud services enable container.googleapis.com
 
-
+#create cluster
 gcloud beta container clusters create $CLUSTER_NAME \
   --project=$PROJECT_ID \
   --cluster-version=latest \
@@ -22,14 +22,24 @@ gcloud beta container clusters create $CLUSTER_NAME \
   --num-nodes=$NUM_NODES \
   --region=$ZONE
 
+#get tfserving image
 docker pull tensorflow/serving
 
 docker tag tensorflow/serving  gcr.io/$PROJECT_ID/$APP_NAME
 
 docker push gcr.io/$PROJECT_ID/$APP_NAME
 
-kubectl apply -f  volume_claim.yaml
+#get resnet
+wget https://tfhub.dev/tensorflow/resnet_50/classification/1?tf-hub-format=compressed -O resnet.tar.gz
+mkdir -p /tmp/resnet/123
+tar xvfz resnet.tar.gz -C /tmp/resnet/123/
+
+#deploy
+kubectl apply -f  tfserving_volume.yaml
 kubectl apply -f  tfserving_configmap.yaml
 kubectl apply -f  tfserving_deployment.yaml
 kubectl apply -f  tfserving_service.yaml
 
+POD_NAME=($(kubectl get pods  --selector=app=tensorflowserving | awk 'NR>1 { printf sep $1; sep=" "}'))
+#copy model
+kubectl cp /tmp/resnet $POD_NAME:/models/
