61c87b33bb695d6e58e3f92a61f4d8823beb3720
zerbini.roberto@gmail.com <-?x0#Szs1W?EICWl>
2022-02-02 17:21:23 +0400

    changed <PROECT_NAME> TO <PROJECT_ID>

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 /bin/bash
 
+cd ./tfserving_k8s
+
 gcloud config set compute/zone us-central1-f
 PROJECT_ID=$(gcloud config get-value project)
 CLUSTER_NAME=my-cluster
@@ -7,6 +9,8 @@ APP_NAME=tfserving
 ZONE=us-central1-f
 ACCOUNT=$(gcloud config list account --format "value(core.account)")
 NUM_NODES=1
+
+sed -i -- 's/<PROJECT_ID>/'"$PROJECT_ID"'/g' tfserving_deployment.yaml
 
 gcloud services enable container.googleapis.com
 
@@ -23,3 +27,9 @@ docker pull tensorflow/serving
 docker tag tensorflow/serving  gcr.io/$PROJECT_ID/$APP_NAME
 
 docker push gcr.io/$PROJECT_ID/$APP_NAME
+
+kubectl apply -f  volume_claim.yaml
+kubectl apply -f  tfserving_configmap.yaml
+kubectl apply -f  tfserving_deployment.yaml
+kubectl apply -f  tfserving_service.yaml
+
